08e4b10a97c867bca11f00a870d29c69d727d1f6
Jonas Bucher <j.bucher.mn@gmail.com>
2018-09-30 14:23:31 +0200

    Intermediate

--- esc.py
+++ esc.py
@@ -14,46 +14,74 @@ def signal_to_dc(signal):
 
 
 class ESC:
-    def __init__(self, name):
+    def __init__(self, name, pin):
         self.name = name
+        self.pin = pin
+        self._pwm = None
 
-    def initialize(self, pin, calibrate=False):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.OUT)
-        self.pwm = GPIO.PWM(pin, PWM_FREQ)
-
-        if calibrate:
-            print("Calibrating %s..." % self.name)
-
-            self.pwm.start(signal_to_dc(1.0))
-            time.sleep(2)
-            self.pwm.ChangeDutyCycle(signal_to_dc(0.0))
-            time.sleep(2)
-
-            print("...done")
-        else:
-            self.pwm.start(signal_to_dc(0.0))
+    def _initialize(self):
+        GPIO.setup(self.pin, GPIO.OUT)
+        self._pwm = GPIO.PWM(self.pin, PWM_FREQ)
 
     def finish(self):
-        self.pwm.stop()
+        self._pwm.stop()
 
     def set_val(self, val):
         if val < 0.:
             val = 0.
         if val > 1.:
             val = 1.
-        self.pwm.ChangeDutyCycle(signal_to_dc(val))
+        self._pwm.ChangeDutyCycle(signal_to_dc(val))
+
+
+class ESCs:
+    def __init__(self):
+        self.escs = []
+
+    def add(self, name, pin):
+        self.escs += [ESC(name, pin)]
+
+    def initialize(self, calibrate=False):
+        GPIO.setmode(GPIO.BCM)
+        for e in self.escs:
+            e._initialize()
+
+        if calibrate:
+            print("Calibrating ESCs...")
+            for e in self.escs:
+                e._pwm.start(signal_to_dc(1.0))
+
+            time.sleep(2.5)
+
+            for e in self.escs:
+                e._pwm.ChangeDutyCycle(signal_to_dc(0.0))
+
+            time.sleep(2.5)
+
+        else:
+            for e in self.escs:
+                e._pwm.start(signal_to_dc(0.0))
+
+    def finish(self):
+        for e in self.escs:
+            e.finish()
 
 
 if __name__ == '__main__':
-    pin = int(input("PIN? "))
-    esc = ESC("PIN%d" % pin)
-    esc.initialize(pin)
+    pins = input("PIN? ").split(",")
+    pins = [int(pin) for pin in pins]
+
+    escs = ESCs()
+    for pin in pins:
+        escs.add("PIN%d" % pin, pin)
+
+    escs.initialize(calibrate=True)
 
     try:
         while True:
             val = int(input("Value in percent? "))
-            esc.set_val(val/100.)
-    except KeyboardInterrupt:
-        esc.finish()
+            for e in escs.escs:
+                e.set_val(val/100.)
+    finally:
+        escs.finish()
         GPIO.cleanup()
