19bb223847579f904e8cf2ce00c7c4f9718d2ad7
Jonas Bucher <j.bucher.mn@gmail.com>
2018-09-30 12:14:32 +0000

    Intermediate

--- esc.py
+++ esc.py
@@ -2,37 +2,53 @@ import RPi.GPIO as GPIO
 import time
 
 
-PWM_FREQ = 50
+PWM_FREQ = 100
+MIN_WIDTH_US = 1064
+MAX_WIDTH_US = 1864
 
 
-def duration_to_dc(duration_ms):
-    full_ms = 1000./PWM_FREQ
-    return 100.*duration_ms/full_ms
+def signal_to_dc(signal):
+    duration_us = MIN_WIDTH_US + signal*(MAX_WIDTH_US - MIN_WIDTH_US)
+    full_us = 1000000./PWM_FREQ
+    return 100.*duration_us/full_us
 
 
 class ESC:
-    def __init__(self, pin):
+    def __init__(self, name):
+        self.name = name
+
+    def initialize(self, pin, calibrate=False):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
         self.pwm = GPIO.PWM(pin, PWM_FREQ)
-        self.pwm.start(duration_to_dc(2.01))
-        time.sleep(1)
-        self.pwm.ChangeDutyCycle(duration_to_dc(0.99))
-        time.sleep(1)
-        self.set_val(0.)
+
+        if calibrate:
+            print("Calibrating %s..." % self.name)
+
+            self.pwm.start(signal_to_dc(1.0))
+            time.sleep(2)
+            self.pwm.ChangeDutyCycle(signal_to_dc(0.0))
+            time.sleep(2)
+
+            print("...done")
+        else:
+            self.pwm.start(signal_to_dc(0.0))
 
     def finish(self):
         self.pwm.stop()
 
     def set_val(self, val):
-        self.pwm.ChangeDutyCycle(duration_to_dc(1. + val))
+        if val < 0.:
+            val = 0.
+        if val > 1.:
+            val = 1.
+        self.pwm.ChangeDutyCycle(signal_to_dc(val))
 
 
 if __name__ == '__main__':
     pin = int(input("PIN? "))
-    print("Initializing ESC on PIN %d..." % pin)
-    esc = ESC(pin)
-    print("...done")
+    esc = ESC("PIN%d" % pin)
+    esc.initialize(pin)
 
     try:
         while True:
--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import RPi.GPIO as GPIO
 from esc import ESC
 import time
 
-pins = [18, 23, 17, 27]
-escs = [ESC(pin) for pin in pins]
+pins = [17, 23, 18, 27]
+escs = [ESC("M%d" % (i+1)) for i, pin in enumerate(pins)]
+
+for e, p in zip(escs, pins):
+    e.initialize(p)
 
 try:
     for i in range(1000):
-        time.sleep(1)
-        print("Thrust on %d" % (i%4))
         escs[(i+1)%4].set_val(0.1)
         escs[(i+2)%4].set_val(0.1)
         escs[(i+3)%4].set_val(0.1)
-        escs[i%4].set_val(0.9)
+        escs[i%4].set_val(0.1)
+        print("Thrust on %d" % (i%4))
+        for j in range(100):
+            escs[i%4].set_val(j/100.)
+            time.sleep(0.1)
+        for j in range(100):
+            escs[i%4].set_val((100.-j)/100.)
+            time.sleep(0.1)
 
 except KeyboardInterrupt:
     for e in escs:
