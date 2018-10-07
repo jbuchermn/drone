7bea090051d40a42fd495fb7038fa11a227b49f5
Jonas Bucher <j.bucher.mn@gmail.com>
2018-10-07 22:22:37 +0000

    Added random parameter to bundle.js; Removed old code

--- webapp/server/server.py
+++ webapp/server/server.py
@@ -1,10 +1,11 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", rand=random.randint(1, 100000))
 
 
 if __name__ == "__main__":
