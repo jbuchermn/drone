d573ad00f9b78910920be8a47fcda72814e374c9
Jonas Bucher <j.bucher.mn@gmail.com>
2018-10-11 16:39:38 +0000

    Moved Janus Endpoint to https

--- webapp/server/server.py
+++ webapp/server/server.py
@@ -1,5 +1,6 @@
 import random
-from flask import Flask, render_template
+import http.client as httplib
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
@@ -7,6 +8,25 @@ app = Flask(__name__, static_folder="../static/dist", template_folder="../static
 def index():
     return render_template("index.html", rand=random.randint(1, 100000))
 
+@app.route("/janus", methods=["GET", "POST", "OPTIONS"])
+@app.route("/janus/<path:p>", methods=["GET", "POST", "OPTIONS"])
+def janus_proxy(p = ""):
+    path = ""
+    if request.query_string:
+        path = "/janus/%s?%s" % (p, request.query_string)
+    else:
+        path = "/janus/" + p
+
+    conn = httplib.HTTPConnection("localhost", 8088)
+    conn.request(request.method, path, body=request.data, headers=dict(request.headers))
+    resp = conn.getresponse()
+    content = resp.read()
+
+    return Response(response=content,
+                    status=resp.status,
+                    headers=dict(resp.headers),
+                    content_type=resp.getheader('content-type'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', ssl_context='adhoc')
--- webapp/static/js/App.js
+++ webapp/static/js/App.js
@@ -14,10 +14,10 @@ export default class App extends React.Component {
                     type="checkbox" 
                     onChange={()=>this.setState({ alive: !this.state.alive })} 
                     defaultChecked={this.state.alive}/> Alive
-                <Signaling alive={this.state.alive} janusEndpoint={'https://janus.conf.meetecho.com/janus'} >
+                <Signaling alive={this.state.alive} janusEndpoint={'/janus'} >
                     <Videostream />
                 </Signaling>
             </div>
         );
-	}
+    }
 }
