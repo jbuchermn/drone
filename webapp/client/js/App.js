9983e69d98bfd804d0cb8fffebc48e568302c53f
Jonas Bucher <j.bucher.mn@gmail.com>
2018-10-22 11:54:23 +0200

    Simple graphing

--- webapp/client/js/App.js
+++ webapp/client/js/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import 'react-vis/dist/style';
+import {
+    XYPlot,
+    XAxis,
+    YAxis,
+    HorizontalGridLines,
+    LineSeries,
+    DiscreteColorLegend
+} from 'react-vis';
 
 import { IP, PORT } from './env';
 import Camera from './Camera';
@@ -12,7 +20,7 @@ export default function App(props){
         <Query query={gql`{ gallery { id, entries { id, kind, name } } }`}>
             {({ loading, error, data }) => {
                 if (loading) return <p>Loading...</p>;
-                if (error) return <p>Error :(</p>;
+                if (error) return <p>Error</p>;
 
                 return data.gallery.entries.map(e => (
                     <p><a 
@@ -20,6 +28,34 @@ export default function App(props){
                         target={"_blank"}>{e.name}</a></p>
                 ));
             }}
+        </Query>,
+        <Query query={gql`{ streams { id, name, t, val } }`} pollInterval={1000}>
+            {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error</p>;
+
+                let time = new Date().getTime()/1000;
+
+                return (
+                    <XYPlot
+                        width={1280}
+                        height={720}>
+                        <HorizontalGridLines />
+                        {data.streams.map(s=>{
+                            let data = s.t.map((t, i) => ({x: t - time, y: s.val[i]}));
+                            return (
+                                <LineSeries
+                                    data={data}/>
+                            );
+
+                        })}
+                        <XAxis />
+                        <YAxis />
+                        <DiscreteColorLegend items={data.streams.map(s=>s.name)} />
+                    </XYPlot>
+                );
+            }}
+
         </Query>
     ];
     
