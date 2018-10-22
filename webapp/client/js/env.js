25199c86b84576b968dde17ae1610265bb1df319
Jonas Bucher <j.bucher.mn@gmail.com>
2018-10-22 12:42:27 +0200

    Fix

--- webapp/client/js/env.js
+++ webapp/client/js/env.js
@@ -1,8 +1,10 @@
-// let ip = location.host;
-//     if(ip.includes(":")){
-//     ip = ip.split(":")[0];
-// }
-// export const IP = ip;
+let ip = location.host;
+let port = 80;
+if(ip.includes(":")){
+    [ip, port] = ip.split(":")[0];
+}
+export const IP = ip;
+export const PORT = port;
 
-export const IP = "172.16.0.110"
-export const PORT = 5000
+// export const IP = "172.16.0.110"
+// export const PORT = 5000
