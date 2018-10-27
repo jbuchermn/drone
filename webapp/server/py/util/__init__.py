9d1a8f157b69060e387db38008fa081220fdb269
Jonas Bucher <j.bucher.mn@gmail.com>
2018-10-27 15:53:53 +0200

    Minor

--- webapp/server/py/util/__init__.py
+++ webapp/server/py/util/__init__.py
@@ -2,4 +2,4 @@ from .bitrate import Bitrate  # noqa F401
 from .non_blocking_file import NonBlockingFile, is_file_write_in_progress  # noqa F401
 from .ping import Ping  # noqa F401
 from .stream import Stream, get_streams  # noqa F401
-from .ws_broadcast import ws_broadcast  # noqa F401
+from .ws_broadcast import WSBroadcast  # noqa F401
