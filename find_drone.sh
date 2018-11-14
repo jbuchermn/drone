818e8c62f9223f9e5e6b9633f30419470f7e7fa2
Jonas Bucher <j.bucher.mn@gmail.com>
2018-11-14 16:15:14 +0100

    Scripts

--- find_drone.sh
+++ find_drone.sh
@@ -5,7 +5,7 @@
 DRONE_IP="172.16.0.127"
 DRONE_FOUND=0
 
-nmcli connection up id $WIFI_CONNECTION
+# nmcli connection up id $WIFI_CONNECTION
 
 echo "Looking in $WIFI_CONNECTION..."
 if ping -c 1 $DRONE_IP > /dev/null 2>&1; then
@@ -16,7 +16,8 @@ else
     nmcli connection down id $WIFI_CONNECTION
 
     echo "Looking for hotspot..."
-    nmcli dev wifi rescan > /dev/null 2>&1
+    # nmcli dev wifi rescan > /dev/null 2>&1
+    nmcli dev wifi list
     if nmcli dev wifi list | grep -q raspberry-drone; then
         echo "...yes"
         nmcli dev wifi connect raspberry-drone password "1234567890"
@@ -35,12 +36,15 @@ fi
 
 if [ "$DRONE_FOUND" = 1 ]; then
     echo "Found drone @ IP: $DRONE_IP"
-    read -p "Command ([s]sh, [f]irefox) " command
+    read -p "Command ([s]sh, [f]irefox, [c]hromium) " command
     if [ $command = s ]; then
         ssh "pi@$DRONE_IP"
     fi
     if [ $command = f ]; then
         firefox "http://$DRONE_IP:5000" &
+    fi
+    if [ $command = c ]; then
+        chromium "http://$DRONE_IP:5000" &
     fi
 else
     echo "Could not find drone"
---
+++ sync_gallery.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+shopt -s nullglob
+
+# Mount
+mkdir /mnt/sdcard
+mount /dev/mmcblk0p1 /mnt/sdcard
+
+# Convert
+for f in /mnt/sdcard/home/pi/Camera/vid/*.avi; do
+    ffmpeg -i $f -vf "transpose=1,transpose=1" ${f%.avi}.mp4
+    rm $f
+done
+
+# Sync
+mkdir -p Camera/vid
+mkdir -p Camera/img
+rsync -av /mnt/sdcard/home/pi/Camera/img ./Camera/img
+rsync -av /mnt/sdcard/home/pi/Camera/vid ./Camera/vid
+
+# Cleanup
+umount /dev/mmcblk0p1
+rm -rf /mnt/sdcard
